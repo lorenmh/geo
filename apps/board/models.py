f85abb058d9e724a6b1b8ab67dfae314878f70eb
Loren <hello@lorenhoward.com>
2015-04-06 23:31:35 -0700

    added board

--- apps/board/models.py
+++ apps/board/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    rid = models.CharField(max_length=16)
+    title = models.CharField(max_length=128)
+    about_text = models.TextField()
+    slug = models.SlugField()
+    created = models.DateTimeField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    
+    # Author = Author
+
+class Message(models.Model):
+    title = models.TextField()
+    text = models.TextField()
